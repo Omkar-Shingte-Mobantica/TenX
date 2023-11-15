33ebf2a14222c6230b9a1347e5e85b20d6e96ec0
OmkarShingte <shingteom@gmail.com>
2023-11-15 17:14:13 +0530

    Initial commit

---
+++ app/src/main/java/com/os/tenxapp/MainActivity.java
@@ -0,0 +1,16 @@
+package com.os.tenxapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNavigation)
+//        bottomNavigationView.itemIconTintList = null
+    }
+}
